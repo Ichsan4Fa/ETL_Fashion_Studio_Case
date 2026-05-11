b3153e0830a00bb948948d675c3fe281e02b5cc2
Ichsan4Fa <90586507+Ichsan4Fa@users.noreply.github.com>
2026-05-11 20:17:03 +0700

    update: transform data

--- utils/transform.py
+++ utils/transform.py
@@ -1,18 +1,15 @@
 import pandas as pd
 import numpy as np
+import timestamp
 
 def transform_to_dataframe(data):
     df = pd.DataFrame(data)
     return df
 
 def transform_data(df,rupiah_exchange):
-    df['Rating'] = df['Rating'].astype(float)
-    df['Colors'] = df['Colors'].astype(int)
-    df['Size'] = df['Size'].astype('string')
-    df['Gender'] = df['Gender'].astype('string')
-    # df['Price_in_USD'] = df['Price'].apply(lambda x: float(x.replace("$", "")))
-    # df['Price_in_IDR'] = (df['Price_in_USD']*rupiah_exchange).astype(float)
-    
-    df['Title'] = df['Title'].astype('string')
-    # df['Price'] = df['Price'].astype(float)
+    df['Price'] = (df['Price']*rupiah_exchange).astype(float)
+    df.drop_duplicates(inplace=True)
+    df.dropna(inplace=True)
+    df = df[df['Title'] != 'Unknown']
+    df['Timestamp'] = timestamp.get_timestamp()
     return df
