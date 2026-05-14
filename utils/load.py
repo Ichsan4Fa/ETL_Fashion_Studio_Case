2fac66afbb35d71c85bf7ae94442a27cec2c4e5d
Ichsan4Fa <90586507+Ichsan4Fa@users.noreply.github.com>
2026-05-14 21:17:01 +0700

    update: error handling and load to gsheets
    error handling on transform

--- utils/load.py
+++ utils/load.py
@@ -1,4 +1,10 @@
+import gspread
+
 from sqlalchemy import create_engine
+from oauth2client.service_account import ServiceAccountCredentials
+
+def load_to_csv(data, file_path):
+    data.to_csv(file_path, index=False)
 
 def load_to_postgres(data, table_name, connector):
     try:
@@ -7,4 +13,17 @@ def load_to_postgres(data, table_name, connector):
             data.to_sql(table_name, con=engine, if_exist='replace',index=False)
             print(f"Data loaded to database succesfully")
     except Exception as e:
-        print(f"Error occurs during loading: {e}")    
+        print(f"Error occurs during loading: {e}")
+
+def load_to_google_sheets(data, spreadsheet_id, sheet_name, credentials):
+    try:
+        scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+        creds = ServiceAccountCredentials.from_json_keyfile_name(credentials, scope)
+        client = gspread.authorize(creds)
+
+        sheet = client.open_by_key(spreadsheet_id).worksheet(sheet_name)
+        sheet.clear()
+        sheet.update([data.columns.values.tolist()] + data.values.tolist())
+        print(f"Data loaded to Google Sheets successfully.")
+    except Exception as e:
+        print(f"Error occurs during loading to Google Sheets: {e}")    
--- utils/transform.py
+++ utils/transform.py
@@ -7,10 +7,16 @@ def transform_to_dataframe(data):
     return df
 
 def transform_data(df,rupiah_exchange):
-    df['Price'] = df['Price'].apply(lambda x: float(x))
-    df['Price'] = (df['Price']*rupiah_exchange).astype(float)
-    df.drop_duplicates(inplace=True)
-    df.dropna(inplace=True)
-    df = df[df['Title'] != 'Unknown']
-    # df['Timestamp'] = datetime.datetime.now()
+    try:
+        df['Price'] = df['Price'].str.replace(',', '').astype(float)
+        df['Price'] = df['Price'].apply(lambda x: float(x))
+        df['Price'] = (df['Price']*rupiah_exchange).astype(float)
+        df['Rating'] = df['Rating'].apply(lambda x: float(x))
+        df['Colors'] = df['Colors'].apply(lambda x: int(x))
+        df.drop_duplicates(inplace=True)
+        df.dropna(inplace=True)
+        df = df[~df['Title'].str.contains('Unknown', case=False, na=False)]
+        df['Timestamp'] = datetime.datetime.now()
+    except Exception as e:
+        print(f"Error occurs during transformation: {e}")
     return df
