e18bdc126a0555eb53518633d7bbefd5fe12f082
Ichsan4Fa <90586507+Ichsan4Fa@users.noreply.github.com>
2026-05-15 21:57:09 +0700

    update: implemented load main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 from utils.extract import fetch_data, scrape_products, extract_fashion_data
 from utils.transform import transform_to_dataframe, transform_data
-from utils.load import load_to_postgres
+from utils.load import load_to_postgres, load_to_csv, load_to_google_sheets
 
 def main():
     try:
@@ -22,13 +22,15 @@ def main():
         except Exception as e:
             print(f"Error occurred while transforming data: {e}")
     else:
-        print("Data transformation successful.")
-    # try:
-    #     load_to_csv(transform_df, "transformed_data.csv")
-    #     print("Data loaded to CSV successfully.")
-    #     load_to_database(transform_df, "fashion_data.db", "postgresql://username:password@localhost:5432/mydatabase")
-    # except Exception as e:
-    #     print(f"Error occurred while loading data: {e}")
+        print("No data to transform.")
+    
+    try:
+        load_to_csv(transform_df, "transformed_data.csv")
+        print("Data loaded to CSV successfully.")
+        load_to_postgres(transform_df, "postgresql://developer:supersecretpassword@localhost:5432/productdb")
+        load_to_google_sheets(transform_df, "1a2b3c4d5e6f7g8h9i0j", "Sheet1", "credentials.json")
+    except Exception as e:
+        print(f"Error occurred while loading data: {e}")
 
 if __name__ == "__main__":
     main()
